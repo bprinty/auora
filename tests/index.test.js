19e1ff223a030b926b07e69ef1b4dd69de7f2471
bprinty <bprinty@gmail.com>
2020-03-23 04:45:56 -0500

    Placeholder for base tests

---
+++ tests/index.test.js
@@ -0,0 +1,29 @@
+/**
+ * Testing for package.
+ */
+
+
+// imports
+// -------
+import { assert } from 'chai';
+
+
+// fixtures
+// --------
+beforeEach(() => {
+  console.log('Starting test session ...');
+});
+afterEach(() => {
+  console.log('Closing test session ...');
+});
+
+
+// tests
+// -----
+describe("main", () => {
+
+  test("placeholder", async () => {
+      assert.isTrue(true);
+  });
+
+});
