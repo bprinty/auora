4869dcd0f2846b35c889ab036e30954d09a8cdaa
bprinty <bprinty@gmail.com>
2020-03-30 21:25:56 -0500

    Added component for labelling things as part of junkyard

---
+++ docs/.vuepress/components/Junkyard.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="junkyard custom-block danger">
+    <p class="custom-block-title">JUNKYARD</p>
+    <p>Below contains pieces from old documentation or incomplete thoughts. Please <strong>remove this section when publishing</strong> this documentation.</p>
+    <hr />
+    <hr />
+    <p>...</p>
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'junkyard',
+};
+</script>
