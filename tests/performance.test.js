d659e29f3a0825f7c475dff9741c65d06ffc7271
bprinty <bprinty@gmail.com>
2020-05-04 18:47:04 -0500

    Added performance criteria for package

--- tests/performance.test.js
+++ tests/performance.test.js
@@ -54,45 +54,44 @@ beforeEach(() => {
 
 // tests
 // -----
+const iterations = 1000;
+
 test("performance.create", async () => {
-  const iterations = 1000;
   const start = Date.now();
   const data = {};
-  for(let i = 0; i < iterations; i += 1) {
+  for (let i = 0; i < iterations; i += 1) {
     store.apply.sync({ foo: 'bar' });
   }
   const delta = Date.now() - start;
-  console.log(`Store: ${delta} ms`);
+  assert.isBelow(delta, 50);
 });
 
 test("performance.update", async () => {
-  const iterations = 1000;
-
   // fill state
-  for(let i = 0; i < iterations; i += 1) {
+  for (let i = 0; i < iterations; i += 1) {
     store.apply.sync({ foo: 'bar' });
   }
 
   // time updates
   const start = Date.now();
-  for(let i = 0; i < iterations; i += 1) {
-    store.apply.sync({ id: i + 1, foo: 'bar' });
+  for (let i = 0; i < iterations; i += 1) {
+    store.apply.sync({ id: i + 1, foo: 'baz' });
   }
   const delta = Date.now() - start;
-  console.log(`Store: ${delta} ms`);
+  assert.isBelow(delta, 50);
 });
 
 test("performance.remove", async () => {
   // fill state
-  for(let i = 0; i < iterations; i += 1) {
+  for (let i = 0; i < iterations; i += 1) {
     store.apply.sync({ foo: 'bar' });
   }
 
   // time remove
   const start = Date.now();
-  for(let i = 0; i < iterations; i += 1) {
+  for (let i = 0; i < iterations; i += 1) {
     store.apply.remove(i + 1);
   }
   const delta = Date.now() - start;
-  console.log(`Store: ${delta} ms`);
+  assert.isBelow(delta, 50);
 });
